7af38aef3bb19c8f5f79fc1324bbc36058e8718b
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-12-10 23:04:07 -0300

    bindValue vs bindParam

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,14 +7,14 @@ require_once 'vendor/autoload.php';
 try {
 
     $pdo = new PDO('sqlite:' . __DIR__ . ' ./banco2sqlite');
-    $student = new Student(null,"Miguel Silva", new DateTimeImmutable('1989-04-11'));
-    $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?,?);";
+    $student = new Student(null,"Jéssica Silva", new DateTimeImmutable('1990-01-08'));
+    $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 
     $message = "Erro ao executar instrução!".PHP_EOL;
     $statement = $pdo->prepare($sqlInsert);
 
-    $statement->bindValue(1,$student->name());
-    $statement->bindValue(2,$student->birthDate()->format("Y-m-d"));
+    $statement->bindValue("name",$student->name());
+    $statement->bindValue("birth_date",$student->birthDate()->format("Y-m-d"));
 
     if($statement->execute()){
         $message = "Executado com sucesso!".PHP_EOL;
